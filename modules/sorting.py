cc8c2ffa92ef1a2905f8ab61efe0e9f14da4a9a8
vlreinier <43336873+vlreinier@users.noreply.github.com>
2020-02-19 19:07:17 +0100

    First code threaded merge sort

--- modules/sorting.py
+++ modules/sorting.py
@@ -62,7 +62,52 @@ def recursive_insertion(element: int, data: List[int]) -> List[int]:
             return [head] + recursive_insertion(element, tail) # else, keep the head separate, and recursively insert into the tail
         
 
-def merge_sort(data: List[int]) -> List[int]:
+def merge_sort(data: List[int]) -> None:
+    merge_sort2(data, 0, len(data)-1)
+
+def merge_sort2(data: List[int], low: int, high: int) -> None:
+    """Split data, sort subarrays and merge them into sorted array."""
+    # test base case size of array equals 1
+    if (high - low) >= 1: # if not base case
+        middle1 = (low + high) // 2 # calculate middle of the array
+        middle2 = middle1 + 1 # calculate next element over
+
+        # split array in half then sort each half (recursive calls)
+        merge_sort2(data, low, middle1) # first half of the array
+        merge_sort2(data, middle2, high) # second half of the array
+
+        # merge two sorted arrays after split calls return
+        merge(data, low, middle1, middle2, high)
+
+# merge two sorted subarrays into one sorted subarray
+def merge(data: List[int], left: int, middle1: int, middle2: int, right: int) -> None:
+    left_index = left # index into left subarray
+    right_index = middle2 # index into right subarray
+    combined_index = left # index into temporary working array
+    merged = [0] * len(data) # working array
+
+    # merge arrays until reaching end of either
+    while left_index <= middle1 and right_index < right:
+        # place smaller of two current elements into result
+        # and move to next space in arrays
+        if data[left_index] <= data[right_index]:
+            merged[combined_index] = data[left_index]
+            combined_index += 1
+            left_index += 1
+        else:
+            merged[combined_index] = data[right_index]
+            combined_index += 1
+            right_index += 1
+
+    # if left array is empty
+    if left_index == middle2: # if True, copy in rest of right array
+        merged[combined_index:right + 1] = data[right_index:right + 1]
+    else: # right array is empty, copy in rest of left array
+        merged[combined_index:right + 1] = data[left_index: middle1 + 1]
+
+    data[left:right + 1] = merged[left:right + 1] # copy back to data
+
+def merge_sort_v2(data: List[int]) -> List[int]:
     if len(data) > 1:
         mid = len(data) // 2
         left = data[:mid]
@@ -103,11 +148,29 @@ def merge_sort(data: List[int]) -> List[int]:
             k += 1
 
 
-def recursive_merge_sort(data: List[int]) -> List[int]:
-    """Recursive merge sort implementation for sorting arrays"""
-    if len(data) == 1: # arrays with 1 element are sorted
-        return data
-    else:
-        middle = int(len(data)/2) # find the middle (round down if len(data) is odd)
-        first, second = data[:middle], data[middle:] # split the list in half
-        return merge_arrays(recursive_merge_sort(first), recursive_merge_sort(second)) # merge_sort both arrays, and merge them into the result
+def merge_sort(xs) -> None:
+
+    unit = 1
+    while unit <= len(xs):
+        h = 0
+        for h in range(0, len(xs), unit * 2):
+            l, r = h, min(len(xs), h + 2 * unit)
+            mid = h + unit
+            # merge xs[h:h + 2 * unit]
+            p, q = l, mid
+            while p < mid and q < r:
+                # use <= for stable merge merge
+                if xs[p] <= xs[q]:
+                    p += 1
+                else:
+                    tmp = xs[q]
+                    xs[p + 1: q + 1] = xs[p:q]
+                    xs[p] = tmp
+                    p, mid, q = p + 1, mid + 1, q + 1
+
+        unit *= 2
+        
+    return xs
+
+def merge_sort_to_array(result_manager, array):
+    return result_manager.append(merge_sort(array))
--- modules/timing.py
+++ modules/timing.py
@@ -1,11 +1,14 @@
 import timeit
 from numpy import mean
 
-def get_timing(function, parameters, repeat=5):
+def get_timing(function, to_iterate, repeat=5):
     string = function.__name__ + ":\n"
     lib = {}
-    for p in parameters:
-        timing = timeit.timeit(lambda: function(p), number=repeat)
-        lib[len(p)] = timing
-        string += "List size: "+str(len(p))+" | "+"Time in sec: "+'{:f}'.format(timing)+"\n"
+    for i in to_iterate:
+        timing = timeit.timeit(lambda: function(i), number=repeat)
+        lib[len(i)] = timing
+        string += "List size: "+str(len(i))+" | "+"Time in sec: "+'{:f}'.format(timing)+"\n"
     return string, lib
+
+def function_timing(function, repeat, *argv):
+    return timeit.timeit(lambda: function(*argv), number=repeat)
